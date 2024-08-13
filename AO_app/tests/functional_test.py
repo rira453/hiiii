8007831a4a0bd6176695716392e6763aee3abb10
sihamidrissi <elidrissisiham2@gmail.com>
2024-08-13 20:09:16 +0100

    tests

--- AO_app/tests/functional_test.py
+++ AO_app/tests/functional_test.py
@@ -1,80 +1,77 @@
-from django.test import TestCase
-from django.urls import reverse
+from django.test import TestCase, Client
 from django.contrib.auth.models import User
+from django.urls import reverse
+from django.core import mail
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
 
-class UserRegistrationTest(TestCase):
 
+
+class AuthenticationTests(TestCase):
     def setUp(self):
-        self.url = reverse('register')  # Replace with your registration URL
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username='testuser',
+            email='testuser@example.com',
+            password='securepassword'
+        )
 
-    def test_successful_registration(self):
-        response = self.client.post(self.url, {
-            'username': 'testuser',
-            'email': 'testuser@example.com',
-            'password1': 'securepassword123',
-            'password2': 'securepassword123'
-        })
-        self.assertEqual(response.status_code, 302)  # Assuming redirect after successful registration
-        self.assertTrue(User.objects.filter(username='testuser').exists())
+    def test_login_success(self):
+        response = self.client.post(reverse('sing_in'), {'email': 'testuser@example.com', 'password': 'securepassword'})
+        self.assertEqual(response.status_code, 302)  
+        self.assertRedirects(response, reverse('index'))
 
-    def test_registration_with_mismatched_passwords(self):
-        response = self.client.post(self.url, {
-            'username': 'testuser2',
-            'email': 'testuser2@example.com',
-            'password1': 'securepassword123',
-            'password2': 'differentpassword'
-        })
-        self.assertEqual(response.status_code, 200)  # Assuming registration page reloads on error
-        self.assertContains(response, "Passwords must match")
+    def test_login_failure(self):
+        response = self.client.post(reverse('sing_in'), {'email': 'testuser@example.com', 'password': 'wrongpassword'})
+        self.assertEqual(response.status_code, 200)  
+        self.assertContains(response, "Mot de passe incorrect")
 
-
-
-class UserLoginTest(TestCase):
-
+class PasswordResetTests(TestCase):
     def setUp(self):
-        self.login_url = reverse('login')  # Replace with your login URL
-        self.user = User.objects.create_user(username='testuser', password='securepassword123')
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username='testuser',
+            email='testuser@example.com',
+            password='securepassword'
+        )
 
-    def test_successful_login(self):
-        response = self.client.post(self.login_url, {
-            'username': 'testuser',
-            'password': 'securepassword123'
+    def test_password_reset_request(self):
+        response = self.client.post(reverse('forgot_password'), {'email': 'testuser@example.com'})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Un email a été envoyé avec les instructions pour réinitialiser votre mot de passe.")
+
+       
+        self.assertEqual(len(mail.outbox), 1)
+        email = mail.outbox[0]
+        self.assertIn('Modification de mot de passe!', email.subject)
+
+    def test_password_reset_confirm(self):
+        
+        token = default_token_generator.make_token(self.user)
+        uid = urlsafe_base64_encode(force_bytes(self.user.pk))
+
+        response = self.client.post(reverse('update_password', kwargs={'uidb64': uid, 'token': token}), {
+            'new_password': 'newsecurepassword',
+            'confirm_password': 'newsecurepassword'
         })
-        self.assertEqual(response.status_code, 302)  # Assuming redirect after successful login
-        self.assertTrue(self.client.login(username='testuser', password='securepassword123'))
 
-    def test_unsuccessful_login(self):
-        response = self.client.post(self.login_url, {
-            'username': 'testuser',
-            'password': 'wrongpassword'
-        })
-        self.assertEqual(response.status_code, 200)  # Assuming login page reloads on error
-        self.assertContains(response, "Invalid credentials")
+        self.assertEqual(response.status_code, 302)  
+        self.assertRedirects(response, reverse('sing_in'))
 
+        # Tester la connexion avec un nouveau mot de passe
+        response = self.client.post(reverse('sing_in'), {'email': 'testuser@example.com', 'password': 'newsecurepassword'})
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('index'))
 
-class UserLogoutTest(TestCase):
-
+class UserActivationTests(TestCase):
     def setUp(self):
-        self.logout_url = reverse('logout')  # Replace with your logout URL
-        self.user = User.objects.create_user(username='testuser', password='securepassword123')
-        self.client.login(username='testuser', password='securepassword123')
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username='testuser',
+            email='testuser@example.com',
+            password='securepassword',
+            is_active=False  #L'utilisateur doit être inactif initialement
+        )
 
-    def test_successful_logout(self):
-        response = self.client.get(self.logout_url)
-        self.assertEqual(response.status_code, 302)  # Assuming redirect after logout
-        self.assertFalse('_auth_user_id' in self.client.session)
-
-
-
-class PasswordResetTest(TestCase):
-
-    def setUp(self):
-        self.email = 'testuser@example.com'
-        self.user = User.objects.create_user(username='testuser', email=self.email, password='securepassword123')
-        self.reset_url = reverse('password_reset')  # Replace with your password reset URL
-
-    def test_successful_password_reset(self):
-        response = self.client.post(self.reset_url, {'email': self.email})
-        self.assertEqual(response.status_code, 302)  # Assuming redirect after successful request
-        # Check for email sent (if your email backend is configured to use console for testing)
-        # or check other aspects of the reset process
+    
---
+++ AO_app/tests/unit_test.py
@@ -0,0 +1,227 @@
+from django.test import TestCase
+from AO_app.models import *
+from AO_app.forms import *
+from django.test import TestCase
+from django.core.exceptions import ValidationError
+
+#Test contact request model
+class ContactRequestModelTest(TestCase):
+
+    def test_string_representation(self):
+        contact_request = ContactRequest(full_name="Siham Idrissi")
+        self.assertEqual(str(contact_request), contact_request.full_name)
+
+    def test_verbose_name(self):
+        self.assertEqual(ContactRequest._meta.verbose_name, "Demande de Contact")
+        self.assertEqual(ContactRequest._meta.verbose_name_plural, "Demandes de Contact")
+
+    def test_create_contact_request(self):
+        contact_request = ContactRequest.objects.create(
+            type_of_request="Demande d'info",
+            company_name="Test Company",
+            industry="Technology",
+            full_name="Siham Idrissi",
+            phone_number="0123456789",
+            email="elidrissisiham@test.com",
+            observations="Test observations"
+        )
+        self.assertEqual(contact_request.type_of_request, "Demande d'info")
+        self.assertEqual(contact_request.company_name, "Test Company")
+        self.assertEqual(contact_request.industry, "Technology")
+        self.assertEqual(contact_request.full_name, "Siham Idrissi")
+        self.assertEqual(contact_request.phone_number, "0123456789")
+        self.assertEqual(contact_request.email, "elidrissisiham@test.com")
+        self.assertEqual(contact_request.observations, "Test observations")
+
+
+
+
+#table data model 
+class TableDataTest(TestCase):
+    def test_valid_instance(self):
+        instance = TableData.objects.create(
+            site='Test Site',
+            numero_ao='12345',
+            designation='Test Designation',
+            categorie='Test Category',
+            date_lancement='2024-01-01',
+            date_remise='2024-01-10',
+            date_ouverture='2024-01-15',
+            estimation_projet_dhht=10000.00,
+            seance_ouverture='Test Seance',
+            pdf_file=None
+        )
+        self.assertIsInstance(instance, TableData)
+        self.assertEqual(instance.numero_ao, '12345')
+
+    def test_invalid_instance(self):
+        
+        instance = ContactRequest(
+            type_of_request='Invalid Type',  
+            observations=''  
+        )
+        with self.assertRaises(ValidationError) as context:
+            instance.full_clean()
+        
+        
+        self.assertIn('type_of_request', context.exception.error_dict)
+        self.assertIn('observations', context.exception.error_dict)
+
+    def test_string_representation(self):
+        instance = ContactRequest(full_name='Siham Idrissi')
+        self.assertEqual(str(instance), 'Siham Idrissi')
+
+#Marche model 
+
+class MarcheTest(TestCase):
+    def test_valid_instance(self):
+        instance = Marche.objects.create(
+            site='Test Site',
+            numero_ao='67890',
+            designation='Test Marche',
+            categorie='Test Category',
+            ouverture_financiere='2024-02-01',
+            montant_dhht=20000.00,
+            attributaire='Test Attributaire',
+            detail='Test Detail'
+        )
+        self.assertIsInstance(instance, Marche)
+        self.assertEqual(instance.numero_ao, '67890')
+
+    def test_string_representation(self):
+        instance = Marche(numero_ao='67890')
+        self.assertEqual(str(instance), '67890')
+
+
+
+#mise a jour mode
+class NewsletterSubscriptionTest(TestCase):
+    def test_valid_instance(self):
+        instance = NewsletterSubscription.objects.create(
+            email='test@example.com'
+        )
+        self.assertIsInstance(instance, NewsletterSubscription)
+        self.assertEqual(instance.email, 'test@example.com')
+
+    def test_string_representation(self):
+        instance = NewsletterSubscription(email='test@example.com')
+        self.assertEqual(str(instance), 'test@example.com')
+
+
+
+#download history model
+class DownloadHistoryTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='password')
+        self.table_data = TableData.objects.create(
+            site='Test Site',
+            numero_ao='12345',
+            designation='Test Designation',
+            categorie='Test Category',
+            date_lancement='2024-01-01',
+            date_remise='2024-01-10',
+            date_ouverture='2024-01-15',
+            estimation_projet_dhht=10000.00,
+            seance_ouverture='Test Seance',
+            pdf_file=None
+        )
+
+    def test_valid_instance(self):
+        instance = DownloadHistory.objects.create(
+            user=self.user,
+            table_data=self.table_data
+        )
+        self.assertIsInstance(instance, DownloadHistory)
+        self.assertEqual(instance.user, self.user)
+
+    def test_string_representation(self):
+        instance = DownloadHistory(user=self.user, table_data=self.table_data)
+        self.assertIn('downloaded', str(instance))
+
+#Forms
+
+class ContactFormTestCase(TestCase):
+    
+    def test_valid_form(self):
+        data = {
+            'type_of_request': 'Demande d\'info',
+            'company_name': 'Test Company',
+            'industry': 'Tech',
+            'full_name': 'Siham Idrissi',
+            'phone_number': '0612345678',
+            'email': 'elidrissisiham@test.com',
+            'observations': 'No observations'
+        }
+        form = ContactForm(data=data)
+        self.assertTrue(form.is_valid())
+    
+    def test_invalid_phone_number(self):
+        data = {
+            'type_of_request': 'Demande d\'info',
+            'company_name': 'Test Company',
+            'industry': 'Tech',
+            'full_name': 'Siham Idrissi',
+            'phone_number': '123456789',
+            'email': 'elidrissisiham@test.com',
+            'observations': 'No observations'
+        }
+        form = ContactForm(data=data)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['phone_number'], ['Veuillez entrer un numéro de téléphone marocain valide.'])
+
+
+class NewsletterFormTestCase(TestCase):
+
+    def test_valid_email(self):
+        data = {'email': 'valid.email@example.com'}
+        form = NewsletterForm(data=data)
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_email(self):
+     data = {'email': 'invalid-email'}
+     form = NewsletterForm(data=data)
+     self.assertFalse(form.is_valid())
+     # Check against the localized error message
+     self.assertEqual(form.errors['email'], ['Saisissez une adresse de courriel valide.'])
+
+
+
+class RegistrationFormTestCase(TestCase):
+
+    def test_valid_form(self):
+     data = {
+        'username': 'SihamIdrissi',
+        'email': 'elidrissisiham@test.com',
+        'password1': 'StrongP@ssw0rd!',
+        'password2': 'StrongP@ssw0rd!',
+        'activite': 'Developer',
+        'categorie': 'Category',
+        'adresse': '123 Street',
+        'ville': 'City',
+        'telephone': '0612345678',
+        'fax': '0612345678'
+    }
+     form = RegistrationForm(data=data)
+     self.assertTrue(form.is_valid(), "Form should be valid, but it's not.")
+
+    
+    def test_duplicate_email(self):
+        User.objects.create_user(username='existinguser', email='elidrissisiham@test.com', password='password123')
+        data = {
+            'username': 'newuser',
+            'email': 'elidrissisiham@test.com',
+            'password1': 'password123',
+            'password2': 'password123',
+            'activite': 'Developer',
+            'categorie': 'Category',
+            'adresse': '123 Street',
+            'ville': 'City',
+            'telephone': '0612345678',
+            'fax': '0612345678'
+        }
+        form = RegistrationForm(data=data)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['email'], ['An account with this email address already exists!'])
+
+
+
